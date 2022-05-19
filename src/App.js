e42609d2ff217391e126c301bad22164ca25ac9b
dimdejesus <ddejesus0320@gmail.com>
2022-05-19 23:15:21 +0800

    Adding expenses and total for my web app data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Box } from "@mui/material";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 import Hidden from "@mui/material/Hidden";
 
@@ -7,19 +7,31 @@ import Header from "./components/Header";
 import Body from "./components/Body";
 
 import useBackend from "./hooks/useBackend";
+import { useAlertContext } from "./hooks/useCustomContext";
 import Space from "./components/utils/Space";
 import FabContainer from "./components/fab/FabContainer";
 
 function App() {
+	const { getAllExpenses } = useBackend();
+	const { triggerError } = useAlertContext();
+
+	const [expenses, setExpenses] = useState([]);
+	const [totalExpenses, setTotalExpenses] = useState(0);
+
 	useEffect(() => {
-		console.log("hello");
-	}, []);
+		getAllExpenses()
+			.then((response) => {
+				setExpenses(response.expenses);
+				setTotalExpenses(response.sumExpenses._sum.amount);
+			})
+			.catch((error) => triggerError(error.message));
+	}, [expenses]);
 
 	return (
 		<Box sx={{ width: "80%", margin: "auto" }}>
 			<Header />
 			<Space />
-			<Body />
+			<Body expenses={expenses} total={totalExpenses} />
 			<Hidden smUp>
 				<FabContainer />
 			</Hidden>
