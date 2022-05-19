b255982bde5f1b9f836ea10ff76b0a80adb0a976
dimdejesus <ddejesus0320@gmail.com>
2022-05-19 23:12:33 +0800

    Setting up Header and Body

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,24 +2,33 @@ import { Grid } from "@mui/material";
 import React from "react";
 
 import CustomContainer from "./utils/CustomContainer";
+import TotalExpensesTracker from "./Body/TotalExpensesTracker";
+import ExpenseWeekForm from "./Body/ExpenseWeekForm";
+import Expenses from "./Body/Expenses";
 
 //Contains the structure of the content. 2 container in left and 1 big container on right.
 //The main container has 2 sections -> left and right
 //the left is an item that is also a container in order to structure the 2 items at the top and bottom.
 //the right is an item that consist of list of expenses
-const Body = () => {
+const Body = ({ expenses, total }) => {
 	return (
 		<Grid container spacing={2}>
-			<Grid item container direction="column" spacing={2} xs={12} sm={6} md={4}>
+			<Grid item container direction="column" spacing={2} xs={12} md={4}>
 				<Grid item>
-					<CustomContainer></CustomContainer>
+					<CustomContainer>
+						<TotalExpensesTracker total={total} />
+					</CustomContainer>
 				</Grid>
 				<Grid item>
-					<CustomContainer></CustomContainer>
+					<CustomContainer>
+						<ExpenseWeekForm expenses={expenses} />
+					</CustomContainer>
 				</Grid>
 			</Grid>
-			<Grid item xs={12} sm={6} md={8}>
-				<CustomContainer></CustomContainer>
+			<Grid item xs={12} md={8}>
+				<CustomContainer>
+					<Expenses expenses={expenses} />
+				</CustomContainer>
 			</Grid>
 		</Grid>
 	);
--- src/components/Header.js
+++ src/components/Header.js
@@ -6,12 +6,15 @@ import Hidden from "@mui/material/Hidden";
 import MUISwitch from "./utils/MUISwitch";
 import { useThemeContext } from "../hooks/useCustomContext";
 
+//This is where the mode switch and the title switch located
 const Header = () => {
 	return (
 		<Grid container justifyContent="space-between" alignItems="center" sx={{ mt: 2 }}>
 			{" "}
 			<Grid item>
-				<Typography variant="h3">Expense Tracker</Typography>
+				<Typography variant="h3" sx={{ fontWeight: "bold" }}>
+					Expense Tracker
+				</Typography>
 			</Grid>
 			<Hidden smDown>
 				<Grid item>
