2c78ac6140ca2ce29540c376964200fbab14ae15
dimdejesus <ddejesus0320@gmail.com>
2022-05-19 23:14:06 +0800

    Adjusting the Container component

--- src/components/utils/CustomContainer.js
+++ src/components/utils/CustomContainer.js
@@ -2,13 +2,13 @@ import { Paper } from "@mui/material";
 import React from "react";
 
 //Container on where the data or form will be placed
-const CustomContainer = () => {
+const CustomContainer = ({ children }) => {
 	return (
 		<Paper
 			elevation={3}
-			sx={{ boxShadow: "0px 8px 24px 2px rgba(54, 59, 100, 0.08)", padding: 10 }}
+			sx={{ boxShadow: "0px 8px 24px 2px rgba(54, 59, 100, 0.08)", padding: 5 }}
 		>
-			Hello ikakaganda ko gash
+			{children}
 		</Paper>
 	);
 };
